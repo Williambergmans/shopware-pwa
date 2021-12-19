d3f36b13c94cdcf39c35d399017b515a5fcecad0
William <william@sition.nl>
2021-12-19 19:29:36 +0100

    pwa config

---
+++ shopware-pwa.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  shopwareEndpoint: "http://pwa-demo.sition.cloud",
+  shopwareAccessToken: "SWSCVZDLSKMZMLO1DFDMSXBIVQ",
+  shopwareApiClient: {
+    // optional, allow to override the default settings
+    timeout: 5000, // timeout limit in ms
+  },
+
+  shopwareDomainsAllowList: [
+    "http://pwa-demo.sition.cloud",
+    "http://pwa-demo.sition.cloud/de",
+  ],
+
+};
